a163f021d3ec59e48f0ba7a3c7501bfd1cfc2c69
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-06-19 17:19:57 +0800

    find password successfully notice.

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use mysql_xdevapi\Session;
 
 class ResetPasswordController extends Controller
 {
@@ -35,5 +37,11 @@ class ResetPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    public function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success', 'Password update success,You are login now！');
+        return redirect($this->redirectPath());
     }
 }
