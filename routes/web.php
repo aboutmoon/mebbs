191bb34652cf5ac3f471ed0f85e892ee63782695
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-06-21 11:23:52 +0800

    editor upload image

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', 'PagesController@root')->name('root');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Auth::routes(['verify' => true]);
+
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
